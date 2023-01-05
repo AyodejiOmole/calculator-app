0edfa50a49a38d6e5d45b080820e92c57de61ce9
Omole Daniel <omoledaniel13@gmail.com>
2023-01-05 12:13:55 +0100

    Finished basic design of buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from 'react';
 import Buttons from './components/Buttons';
+import { symbols } from './assets/symbols';
 
-const symbols = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "=", "/"];
 function App() {
-
   const [figures, setFigures] = useState([]);
 
   const handleClick = (symbol) => {
@@ -22,17 +21,30 @@ function App() {
   //   console.log(figures.join(""));
   // }, [figures]);
 
+  const checkNumber = (symbol) => {
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    if(numbers.includes(Number(symbol))) {
+      // console.log(symbol);
+      return true;
+    }
+    // console.log(symbol);
+    return false;
+ }
+
   return (
     <div className='lg:mx-5'>
-      <div className='lg:w-[30%] md:w-[50%] mt-20 w-[90%] bg-slate-100 dark:bg-slate-900 mx-auto rounded-md shadow-lg'>
-        <button onClick={toggleMode}>
-          Switch
-        </button>
-        <p>{figures.join("")}</p>
-        <div className='grid grid-cols-4'>
+      <div className="flex justify-between px-20 align-center py-3">
+          <h1 className='font-bold text-3xl text-slate-800 dark:text-white italic'>Calculator</h1>
+          <div onClick={toggleMode} className="flex justify-end align-center">
+            Switch
+          </div>
+        </div>
+      <div className='lg:w-[35%] mt-5 md:w-[50%] w-[90%] bg-slate-100 dark:bg-slate-900 py-3 px-5 mx-auto rounded-md shadow-lg'>
+        <input disabled className="font-semibold py-3 dark:text-white text-slate-800 my-12 text-base w-full border border-red-400" value={figures.join("")}/>
+        <div className='grid grid-cols-4 grid-rows-5'>
           {symbols.map((symbol, index) => {
-            return <div key={index}>
-              <Buttons symbol={symbol} handleClick={handleClick}/>
+            return <div key={index} className={symbol === "=" ? "grid row-span-2" : ""}>
+              <Buttons symbol={symbol} handleClick={handleClick} number={checkNumber(symbol) ? true : false}/>
             </div>
           })}
         </div>
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Buttons = ({symbol, handleClick}) => {
+const Buttons = ({symbol, handleClick, number}) => {
   return (
-    <div onClick={() => handleClick(symbol)} className={`flex w-auto p-4 cursor-pointer duration-1000 transition-all hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-900 hover:border hover:border-red-400 dark:hover:border-slate-600 m-5 shadow-lg align-center justify-center bg-red-400 rounded-full text-white dark:text-white dark:bg-slate-600`}>
+    <div onClick={() => handleClick(symbol)} className={`py-4 px-1 cursor-pointer duration-1000 transition-all hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-900 hover:border grid place-content-center hover:border-red-400 dark:hover:border-slate-600 m-2 shadow-lg ${number ? "bg-blue-400 dark:bg-slate-600" : "bg-red-300"}  text-white dark:text-white`}>
       {symbol}
     </div>
   )
